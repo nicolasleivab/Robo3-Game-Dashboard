c5e6b7f8c6df53e56fae41379ad880c9ea1e0778
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-03-21 18:06:04 +0100

    deleted files

--- tabletop.js
+++
@@ -1,8 +0,0 @@
-function init() {
-  Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1-zYRyDdNQsO1PFA8TivQQvh_CkDZx3lrjUIzoyXsjp4/pubhtml',
-                   callback: function(data, tabletop) { 
-                       console.log(data)
-                   },
-                   simpleSheet: true } )
-}
-window.addEventListener('DOMContentLoaded', init)
