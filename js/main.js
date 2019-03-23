8940b6b2a09b782fb20a076195e6061c3fcb448d
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-03-23 18:07:49 +0100

    update function

--- js/main.js
+++ js/main.js
@@ -11,85 +11,127 @@ var margin = { left:80, right:20, top:50, bottom:100 };
 
 var width = 600 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
+
+var flag = true;
+
+var t = d3.transition().duration(750);
     
 var g = d3.select("#chart-area")
     .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
     .append("g")
-        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")"); //translating our chart
+        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+var xAxisGroup = g.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height +")");
+
+var yAxisGroup = g.append("g")
+    .attr("class", "y axis");
+
+// X Scale
+var x = d3.scaleBand()
+    .range([0, width])
+    .padding(0.2);
+
+// Y Scale
+var y = d3.scaleLinear()
+    .range([height, 0]);
 
 // X Label
 g.append("text")
     .attr("y", height + 50)
     .attr("x", width / 2)
     .attr("font-size", "20px")
-    .attr("text-anchor", "middle") //align
+    .attr("text-anchor", "middle")
     .attr("transform", "translate(" + margin.left + ", " + margin.top +  ")")
     .text("Level");
 
 // Y Label
-g.append("text")
+var yLabel = g.append("text")
     .attr("y", -60)
     .attr("x", -(height / 2))
     .attr("font-size", "20px")
-    .attr("text-anchor", "middle") //align
-    .attr("transform", "rotate(-90)") //rotate label along y scale
-    .text("Played Time");
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .text("Revenue");
 
-
-     
-
-    // Clean data: string to integer
-    data.forEach(function(d) {
-        d.sumtime = +d.sumtime; d.num = +d.num;
-    });
 console.log(data);
 
-const MAX = d3.max(data, function(d) { return d.sumtime }); // assigning d3.max to MAX constant
+    // Clean data
+    data.forEach(function(d) {
+        d.sumtime = +d.sumtime;
+        d.num = +d.num;
+       
+    });
 
-    // X Scale
-    var x = d3.scaleBand()
-        .domain(data.map(function(d){ return d.level })) // data.map to get the domain of our x in scaleBand 
-        .range([0, width])
-        .padding(0.3);
+    d3.interval(function(){
+        var newData = flag ? data : data.slice(0);
 
-    // Y Scale
-    var y = d3.scaleLinear()
-        .domain([0, MAX])
-        .range([height, 0]);
+        update(newData)
+        flag = !flag
+    }, 1000);
+
+    // Run the vis for the first time
+    update(data);
+
+
+function update(data) {
+    var value = flag ? "sumtime" : "num";
+
+    x.domain(data.map(function(d){ return d.level }));
+    y.domain([0, d3.max(data, function(d) { return d[value] })])
 
     // X Axis
     var xAxisCall = d3.axisBottom(x);
-    g.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height +")")
-        .call(xAxisCall)
-        .selectAll("text") 
+    xAxisGroup.transition(t).call(xAxisCall).selectAll("text") 
             .style("text-anchor", "end")
             .attr("dx", "-.8em")
             .attr("dy", ".15em")
-            .attr("transform", "rotate(-40)");
+            .attr("transform", "rotate(-40)" 
+                );;;
 
     // Y Axis
     var yAxisCall = d3.axisLeft(y)
-        .tickFormat(function(d){ return d + "min"; });
-    g.append("g")
-        .attr("class", "y axis")
-        .call(yAxisCall);
+        .tickFormat(function(d){ return d; });
+    yAxisGroup.transition(t).call(yAxisCall);
 
-    // Bars
+    // JOIN new data with old elements.
     var rects = g.selectAll("rect")
-        .data(data);
-        
+        .data(data, function(d){
+            return d.level;
+        });
+
+    // EXIT old elements not present in new data.
+    rects.exit()
+        .attr("fill", "green")
+    .transition(t)
+        .attr("y", y(0))
+        .attr("height", 0)
+        .remove();
+
+    // ENTER new elements present in new data...
     rects.enter()
         .append("rect")
-            .attr("class", "bars")
-            .attr("y", function(d){ return y(d.sumtime); })
+            .attr("fill", "green")
+            .attr("y", y(0))
+            .attr("height", 0)
             .attr("x", function(d){ return x(d.level) })
-            .attr("height", function(d){ return height - y(d.sumtime); })
             .attr("width", x.bandwidth)
-            .attr("fill", "#00994d");
+            // AND UPDATE old elements present in new data.
+            .merge(rects)
+            .transition(t)
+                .attr("x", function(d){ return x(d.level) })
+                .attr("width", x.bandwidth)
+                .attr("y", function(d){ return y(d[value]); })
+                .attr("height", function(d){ return height - y(d[value]); });
+
+    var label = flag ? "Played Time (min)" : "Times Played";
+    yLabel.text(label);
+
+}
+
 
             //** end of D3 script **//
 
