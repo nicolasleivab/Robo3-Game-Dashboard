9a31eb5b05d3ab3a5ce53803dd464f7629a48d2f
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-06-04 22:35:08 +0200

    data format

--- js/groupedchart.js
+++ js/groupedchart.js
@@ -9,7 +9,31 @@ function init() {
 
 //** D3.js code **//
 
+//* Filter and format data *//
+var data = data.filter(function(d){return d.ID == '10574525';});
 
+    data.forEach(function(d) {
+        d.Functions = +d.Functions;
+        d.Loops = +d.Loops;
+        d.Movement = +d.Movement;
+        d.PickDrop = +d.PickDrop;
+        d.Cycles = +d.Cycles;
+        d.minL = +d.minL;
+        d.avgL = +d.avgL;
+        d.minF = +d.minF;
+        d.avgF = +d.avgF;
+        d.minC = +d.minC;
+        d.avgC = +d.avgC; 
+        d.minP = +d.minP;
+        d.avgP = +d.avgP;
+        d.minM = +d.minM;
+        d.avgM = +d.avgM;
+
+    });
+
+var instructions = ['Functions', 'Loops', 'Cycles', 'Movement', 'PickDrop'];
+
+var selected = instructions[0];
 
 
 
