af738c3189fd5a2a3800e3ee1442003cb31d4432
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-06-13 17:36:14 +0200

    filter and format data

---
+++ tutor_dashboard/js/area_chart.js
@@ -0,0 +1,46 @@
+/* Stacked Area Chart JS */
+
+//** tabletop init function **//
+function init() {     
+  Tabletop.init( { key: '10g_TGtruCriERlXJurPZQk76pvk30U0pkWgbbfzPrjA', //google sheet key
+                   callback: function(data, tabletop) { 
+                       console.log(data)
+
+
+//* Filter and format data *//
+var data = data.filter(function(d){return d.ID == '10574525';});
+
+var parseDate = d3.timeParse("%m/%d/%Y");
+
+    data.forEach(function(d) {
+        d.date = parseDate(d.date);
+        d['Best Solution'] = d['Best Solution'];
+        d.Cycles = +d.Cycles;
+
+
+    });
+
+ var levels = d3.map(data, function(d){return(d.level)}).keys();
+
+ var selection = levels[0];
+
+
+ //*Chart code*//
+
+
+
+
+
+
+
+
+
+
+
+            //** end of D3 script **//
+
+                   },
+                   simpleSheet: true } )
+}
+window.addEventListener('DOMContentLoaded', init)
+//** end of tabletop init function **//
