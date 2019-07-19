c4f9f6c023cb8893b01ff0008d6a022d219c93ba
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-07-20 00:06:51 +0200

    leaderboard sort func

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -2,36 +2,30 @@
 
 //** tabletop init function **//
 function init() {     
-  Tabletop.init( { key: '10g_TGtruCriERlXJurPZQk76pvk30U0pkWgbbfzPrjA', //google sheet key
+  Tabletop.init( { key: '1evjoQPchLR8iUhjQQ8i56hy6Df5z7K_eVSWs8yVugC4', //google sheet key
                    callback: function(data, tabletop) { 
                        console.log(data)
 
 //** Leaderboard script **//
 
+// Create and sort array of players from data
+
+var Players = data.sort((aPlayer, bPlayer) => aPlayer.Cycles - bPlayer.Cycles);;
+
+var topPlayers = Players.slice(0, 3);
 
 
-//Adapted from https://stackoverflow.com/questions/52507871/creating-a-leaderboard-in-html-js
- function Player(myName, myDate, myScore) {
-        this.name = myName;
-        this.date = myDate;
-        this.score = myScore;
-    }
+console.log(topPlayers);
 
-    // Create new players
-    player1 = new Player("10101010", "01/23/18", 201);
-    player2 = new Player("10574525", "03/24/17", 943);
-    player3 = new Player("11111111", "06/04/18", 79); 
-    Players = [player1, player2, player3];
 
-    function displayLeaderboard() {
+// Display table func adapted from https://stackoverflow.com/questions/52507871/creating-a-leaderboard-in-html-js
+function displayLeaderboard() {
         let theExport = ""; 
-        Players.sort((aPlayer, bPlayer) => bPlayer.score - aPlayer.score);
-        Players.forEach((player) => theExport += '<tr><td>' + player.name + '</td><td>' + player.score + '</td><td>' + player.date + '</td></tr>');
-        document.getElementById("leaderboard").innerHTML = theExport; 
+        topPlayers.forEach((player) => theExport += '<tr><td>' + player.ID + '</td><td>' + player.Cycles + '</td><td>' + player.Instructions + '</td></tr>');
+        document.getElementById("leaderboard").innerHTML = theExport;
     }
 
-    displayLeaderboard(Players);
-
+displayLeaderboard(topPlayers);
 
 
 
