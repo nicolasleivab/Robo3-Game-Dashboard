c7b6fdf74b6d726af9a7d67488c949273f0a2531
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-07-20 23:54:06 +0200

    leaderboard groupby

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -9,29 +9,31 @@ function init() {
 //** Leaderboard script **//
 
 // Create and sort array of players from data
-var groupBy = function(data, key) { // `data` is an array of objects, `key` is the key (or property accessor) to group by
-  // reduce runs this anonymous function on each element of `data` (the `item` parameter,
-  // returning the `storage` parameter at the end
-  return data.reduce(function(storage, item) {
-    // get the first instance of the key by which we're grouping
+// groupBy function adapted from Rob Mathers https://gist.github.com/robmathers/1830ce09695f759bf2c4df15c29dd22d
+
+
+function groupBy(arr, key) { 
+  
+return data.reduce(function(storage, item) {
+
     var group = item[key];
     
-    // set `storage` for this instance of group to the outer scope (if not empty) or initialize it
+
     storage[group] = storage[group] || [];
     
-    // add this item to its group within `storage`
+  
     storage[group].push(item);
     
-    // return the updated storage to the reduce function, which will then loop through the next 
+    
     return storage;
-
-  }, {}); // {} is the initial value of the storage
+  }, {}); 
 };
 
-console.log(groupBy(data, "ID"));
+var group = groupBy(data, "ID");
+console.log(group);
 
 
-var Players = data.sort((aPlayer, bPlayer) => aPlayer.Cycles - bPlayer.Cycles);;
+var Players = data.sort((aPlayer, bPlayer) => aPlayer.Cycles - bPlayer.Cycles);
 
 var topPlayers = Players.slice(0, 3);
 
