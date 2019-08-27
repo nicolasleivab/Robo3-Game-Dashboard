6b5d486102227d1a3862e8fbddd38e5af6ba8858
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-08-28 00:48:36 +0200

    sum func optimized

--- js/heatmapsettings.js
+++ js/heatmapsettings.js
@@ -10,39 +10,36 @@ data.forEach(function(d) {
 	d["Success Probability"] = +d["Success Probability"];
 	d.ID = +d.ID;
 	d.date = parseDate(d.date);
-
     });
 
 personCode = Number(personCode);
 let newData = data.filter(function(d){return d.ID == personCode;});
 
 function sumAll(arr) {
-	let sums = {}, counts = {}, value = [], name;
+	let sums = {}, value = [], mapDate;
 	for (var i = 0; i < arr.length; i++) { //loop through array
-		name = new Date(arr[i].date).valueOf()/1000;
-		if (!(name in sums)) {
-			sums[name] = 0;
-			counts[name] = 0;
+		mapDate = new Date(arr[i].date).valueOf()/1000; //new date in unix timestamp
+		if (!(mapDate in sums)) {
+			sums[mapDate] = 0; 
 		}
-		sums[name] += arr[i]['Success Probability'];
-		counts[name]++;
+		sums[mapDate] += arr[i]['Success Probability']; //sum of values per date
 		}
 								
-	for(name in sums) {
-		value.push({ date: name, value: sums[name]}); //push elements to new array with averages per level
+	for(mapDate in sums) {
+		value.push({ date: mapDate, value: sums[mapDate]}); //push elements to new array with dates and values
 		}
-		return value;
+		return value; //return array of objects
 }
 
 let newData2 = sumAll(newData);
 			
-var parser = function(data) {
-							var stats = {};
-							for (var d in data) {
-								stats[data[d].date] = data[d].value;
-							}
-							return stats;
-						};
+var parser = function(data) { //calheatmap parser function
+	var stats = {};
+		for (var d in data) {
+			stats[data[d].date] = data[d].value;
+			}
+	return stats;
+};
 
 var data2 = parser(newData2);
 
