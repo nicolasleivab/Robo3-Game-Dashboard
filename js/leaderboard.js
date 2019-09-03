c100cefb0aae1f29293132c34e9d9ee56b582684
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-09-03 02:04:41 +0200

    sumAll func

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -22,70 +22,39 @@ data.forEach(function(d) {
    
       });
 
-// Group and sort array of players from data with their Cycles attribute summed up
-// DataGrouper function adapted from Scott Sauyet https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects
-
-
-var DataGrouper = (function() {
-  var has = function(obj, target) {
-      return _.any(obj, function(value) {
-          return _.isEqual(value, target);
-      });
-  };
-
-  var keys = function(data, names) {
-      return _.reduce(data, function(memo, item) {
-          var key = _.pick(item, names);
-          if (!has(memo, key)) {
-              memo.push(key);
+function sumAll(arr) {
+    let sums = {}, value = [], studentID;
+        for (var i = 0; i < arr.length; i++) { //loop through array
+          studentID = arr[i].ID 
+          if (!(studentID in sums)) {
+            sums[studentID] = 0; 
           }
-          return memo;
-      }, []);
-  };
+          sums[studentID] += arr[i]['Cycles'];
+          sums[studentID]  += arr[i]['Instructions']; //sum Score (Cycles + Instructions)
+          }
+                      
+        for(studentID in sums) {
+          value.push({ ID: studentID, Score: sums[studentID]}); //push elements to new array students and score
+          }
+    return value; //return array of objects
+}
+      
+const sumPlayers = sumAll(data);
+      
+const sortedPlayers = sumPlayers.sort((aPlayer, bPlayer) => aPlayer.Score - bPlayer.Score); //sorts the players by score (ascending)
 
-  var group = function(data, names) {
-      var stems = keys(data, names);
-      return _.map(stems, function(stem) {
-          return {
-              key: stem,
-              vals:_.map(_.where(data, stem), function(item) {
-                  return _.omit(item, names);
-              })
-          };
-      });
-  };
+for(i = 0; i < sortedPlayers.length; i++) {
+    sortedPlayers[i].ranking = i+1; //Add ranking
+}
 
-  group.register = function(name, converter) {
-      return group[name] = function(data, names) {
-          return _.map(group(data, names), converter);
-      };
-  };
+const topPlayers = sortedPlayers.slice(0, 3); //filters the first 3 players
 
-  return group;
-}());
-
-DataGrouper.register("sum", function(item) {
-  return _.extend({}, item.key, {Score: _.reduce(item.vals, function(memo, node) {
-      return memo + Number(node.Cycles + node.Instructions); //total score defined by total number of Cycles and Instructions
-  }, 0)});
-});
-
-var sumPlayers = DataGrouper.sum(data, ["ID"]); //assign to sumPlayers the array with the players grouped by score
-console.log(sumPlayers);
-
-var sortedPlayers = sumPlayers.sort((aPlayer, bPlayer) => aPlayer.Score - bPlayer.Score); //sorts the players by score (ascending)
-
-var topPlayers = sortedPlayers.slice(0, 3); //filters the first 3 players
-
-
-console.log(topPlayers);
-
+console.log(sortedPlayers);
 
 // Display table func adapted from https://stackoverflow.com/questions/52507871/creating-a-leaderboard-in-html-js
 function displayLeaderboard() {
     let theExport = ""; //initialize the export
-    var i=1; // counter for ranking position
-    topPlayers.forEach((player) => theExport += '<tr><td>' + i++ + '</td><td>' + player.ID + '</td><td>' + player.Score + '</td></tr>'); //prints the row tables
+    topPlayers.forEach((player) => theExport += '<tr><td>' + player.ranking + '</td><td>' + player.ID + '</td><td>' + player.Score + '</td></tr>'); //prints the row tables
     document.getElementById("leaderboard").innerHTML = theExport;
 }
 
@@ -94,12 +63,14 @@ displayLeaderboard(topPlayers);
 //JQuery show ranking func
 $(document).ready(function() {
     $('span').click(function() {
-        var showYourRanking = sortedPlayers.slice(0, 4); //to be replaced by a ID filter once the login page is setup
-        let theExport = "";
-        var i=1;
-        showYourRanking.forEach((player) => theExport += '<tr><td>' + i++ + '</td><td>' + player.ID + '</td><td>' + player.Score + '</td></tr>');
+        if(personCode){
+        let theExport = ""; //initialize the export
+        topPlayers.forEach((player) => theExport += '<tr><td>' + player.ranking + '</td><td>' + player.ID + '</td><td>' + player.Score + '</td></tr>'); //prints the row tables
         document.getElementById("leaderboard").innerHTML = theExport;
-        });
+    }else{
+
+    }
+    });
 });
 
 
