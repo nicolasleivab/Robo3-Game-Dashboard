4c3c4f75b24ac4e84b61a991ca7ac4de1e8c22a4
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2019-11-02 00:12:12 +0100

    login check data

--- js/inputlogin.js
+++ js/inputlogin.js
@@ -2,26 +2,29 @@ let studentsArray = JSON.parse(localStorage.getItem('allStudents'));
 
 function check(form) { //function to check personCode
 let code = Number(form.personCode.value);
-
+if(localStorage.getItem("allStudents") && localStorage.getItem("data1") && localStorage.getItem("data2") != null){
     if(studentsArray.includes(code)){
-    let form = document.getElementById("studentForm");
-    form.addEventListener("reset", function(){
-    let personCode = document.getElementById('personCode').value;
-    localStorage.setItem( 'objectToPass', personCode );
-        
-    window.location = 'student.html'; //pass person code input value to student dashboard page
-    });  
-    }
-    else {
-    alert("Invalid Person Code")/*displays error message*/  
-    }
-}
-
-//in case the user clears data storage
-$(document).ready(function() {
-    $('input').click(function() {
-        if(localStorage.getItem("allStudents") || localStorage.getItem("data1") || localStorage.getItem("data2") == null){
-            window.location.href = "index.html";
+        let form = document.getElementById("studentForm");
+        form.addEventListener("reset", function(){
+        let personCode = document.getElementById('personCode').value;
+        localStorage.setItem( 'objectToPass', personCode );
+            
+        window.location = 'student.html'; //pass person code input value to student dashboard page
+        });  
         }
-    });
-});
+        else {
+        alert("Invalid Person Code")/*displays error message*/  
+        } 
+}else{//in case the user clears data storage
+    window.location.href = "index.html";
+}
+};
+//check if data is available when login as a tutor
+function checkTutor(){
+    if(localStorage.getItem("allStudents") && localStorage.getItem("data1") && localStorage.getItem("data2") != null){
+        window.location.href = "tutor.html";
+    }else{//in case the user clears data storage
+        window.location.href = "index.html";
+}
+};
+document.getElementById('tutorSubmit').addEventListener('click', checkTutor);
