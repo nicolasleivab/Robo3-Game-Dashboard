300d8e2d337256c696d76ef6fbe2137800f01b82
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2020-01-05 17:58:35 -0300

    GroupedChart component

--- v2.0/src/App.js
+++ v2.0/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.module.css';
 import Layout from './components/Layout/Layout';
+import GroupedChart from './components/GroupedChart/GroupedChart';
 
 function App() {
   return (
     <div>
       <Layout>
+          <GroupedChart/>
       </Layout>
     </div>
   );
---
+++ v2.0/src/components/GroupedChart/GroupedChart.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import ReactApexChart from "react-apexcharts";
+import styles from './GroupedChart.module.css';
+
+// data template
+const sheetsData = [];
+
+class GroupedChart extends React.Component {
+
+state = {
+
+    series: [{
+        name: 'Net Profit',
+        data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+    }, {
+        name: 'Revenue',
+        data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+    }, {
+        name: 'Free Cash Flow',
+        data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+    }],
+    options: {
+        chart: {
+            type: 'bar',
+            height: 350
+        },
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                columnWidth: '55%',
+                endingShape: 'rounded'
+            },
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            show: true,
+            width: 2,
+            colors: ['transparent']
+        },
+        xaxis: {
+            categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+        },
+        yaxis: {
+            title: {
+                text: '$ (thousands)'
+            }
+        },
+        fill: {
+            opacity: 1
+        },
+        tooltip: {
+            y: {
+                formatter: function (val) {
+                    return "$ " + val + " thousands"
+                }
+            }
+        }
+    },
+
+
+};
+
+render() {
+    return (
+
+        <div id="chart" className={styles.GroupedChart}>
+            <ReactApexChart options={this.state.options} series={this.state.series} type="bar" height={350} />
+        </div>
+    );
+  }
+}
+
+export default GroupedChart;
