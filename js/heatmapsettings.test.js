d571ce8d1e041ef4696fac786a750d770a7394b8
Nicolás Leiva Büchi <nicolasleivab@gmail.com>
2020-02-19 19:37:07 -0300

    v2.0.0 React version

--- js/heatmapsettings.test.js
+++
@@ -1,6 +0,0 @@
-const { sumAll } = require('./heatmapsettings');
-
-test('should output date in unix timestamp and sum value', () => {
-    const arrOfObj = sumAll([{date: '01/24/2019', "Success Probability": 1}, {date: '01/24/2019', "Success Probability": 1}]);
-    expect(arrOfObj).toBe('[(date: 1550962800, value: 2)]');
-});
